f4ec6ee275cfeee0091b98dbb957dd2b76d85598
Harold <hgarner213@gmail.com>
2023-08-20 02:01:56 -0500

    Nav Bar Prep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CircleButtonComponent } from './circle-button/circle-button.component';
+import { SectionAComponent } from './section-a/section-a.component';
+import { SectionBComponent } from './section-b/section-b.component';
+import { SquareButtonComponent } from './square-button/square-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CircleButtonComponent,
+    SectionAComponent,
+    SectionBComponent,
+    SquareButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+  NavBarList: any[] = [
+    {
+      "item": "About Me"
+    },
+    {
+      "item": "Projects"
+    },
+    {
+      "item": "Experience"
+    },
+    {
+      "item": "Contact Me"
+    },
+    {
+      "item": "Resume"
+    }
+  ];
+}
