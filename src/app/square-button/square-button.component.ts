443ee80cbaa542eca334e14bd06eb03d9d117141
MugenZeta <hgarner213@gmail.com>
2023-08-24 05:40:47 -0500

    Button done

--- src/app/square-button/square-button.component.ts
+++ src/app/square-button/square-button.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import {style} from "@angular/animations";
 
+// @ts-ignore
 @Component({
   selector: 'app-square-button',
   templateUrl: './square-button.component.html',
   styleUrls: ['./square-button.component.css']
 })
 export class SquareButtonComponent {
-  isOn: boolean = false;
-
-
 }
