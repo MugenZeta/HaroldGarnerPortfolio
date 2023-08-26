532e2a09f0c017ef1cf2455f5b681631b1243f76
Harold <hgarner213@gmail.com>
2023-08-26 02:14:00 -0500

    Button Prep

--- src/app/square-button/square-button.component.ts
+++ src/app/square-button/square-button.component.ts
@@ -8,4 +8,18 @@ import {style} from "@angular/animations";
   styleUrls: ['./square-button.component.css']
 })
 export class SquareButtonComponent {
+  iconList : any[] = [
+    {
+      "name": "LinkedIn",
+      "url": "https://www.linkedin.com/in/harold-g-357923121/",
+      "iconSource": "/src/site_assets/icons/linkedin-112.svg",
+      "iconAltText": "LinkedIn"
+    },
+    {
+      "name": "Github",
+      "url": "https://github.com/MugenZeta",
+      "iconSource": "/src/site_assets/iconmonstr-github-3.svg",
+      "iconAltText": "Github"
+    }
+  ]
 }
