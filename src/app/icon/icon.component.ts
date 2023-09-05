0165a703f6b09f8795a2bb851348c5e1a19afb99
Harold <hgarner213@gmail.com>
2023-09-05 07:10:39 -0500

    Navigation Tweeks

--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { icons } from "../../app-helpers/icon-loader";
+import { IconsService } from "../../app-helpers/icons.service";
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.css']
 })
-export class IconComponent {
+export class IconComponent implements OnInit{
+  constructor(private iService: IconsService) {}
+
+  ngOnInit() {
+    this.getIcons();
+  }
+
+  getIcons(): void {
+    this.iService.getIcons();
+  }
 
 }
