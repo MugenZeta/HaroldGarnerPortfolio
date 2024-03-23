b4fdf90e79871e3163a736cf705b217f0559559c
MugenZeta <hgarner213@gmail.com>
2024-03-23 17:04:27 -0500

    Set up github info

--- src/services/project-service/project.service.ts
+++ src/services/project-service/project.service.ts
@@ -13,43 +13,4 @@ export class ProjectService {
   getProjects(): Observable<any> {
       return this.http.get<any>('https://api.github.com/users/MugenZeta/repos');
   }
-
-  /*
-  name : string = "";
-  getName() : string{return this.name;}
-  setName(name: string):string {
-    return this.name = name;
-  }
-  // @ts-ignore
-
-  getProjectByName(name: string) {
-    name = this.getName();
-    for(let index = 0; index < PROJECT_INFO.length; index++) {
-      if (name === PROJECT_INFO[index].projectName) {
-        return PROJECT_INFO[index];
-
-      } else if(index == PROJECT_INFO.length || name === "") {
-        this.setName("TEST TITLE 1");
-        this.getProjectByName("TEST TITLE 1");
-      }
-    }
-  }
-
-  getProjectDescription(name: string): string {
-    name = this.getName()
-    // @ts-ignore
-    return this.getIconByName(name).iconSource;
-  }
-
-  getIconDataURL(name: string): string {
-    name = this.getName()
-    // @ts-ignore
-    return this.getIconByName(name).iconUrl
-  }
-
-  public getIconDataAltText(name: string): string{
-    name = this.getName();
-    // @ts-ignore
-    return this.getIconByName(name).iconAltText
-  }*/
 }
